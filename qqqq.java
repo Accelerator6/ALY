6d0be50ca2abfdd775b1e4de108f2561037d73fe
Accelerator6 <779365502@qq.com>
2017-03-01 19:13:08 -0600

    JAVA-空心菱形

---
+++ qqqq.java
@@ -0,0 +1,57 @@
+package test;
+import java.util.Scanner;
+
+public class qqqq{	
+	public void Diamond(int n){
+		
+		
+		/*
+			读取参数n 菱形的上半部分用f1接收 下半部分用f2接收
+		*/
+		if(n%2==0)
+			--n;
+		int f1 = n/2+1;
+		int f2 = n/2;
+		int i ,j,k,m,q;
+		for(i=1;i<=f1;i++){	//共有f1次外层循环，用来刻画菱形的上半部分
+			for(m=0;m<f1-i;m++){	//输出第一段空格   
+				System.out.print(" ");
+			}
+			System.out.print("*");  //输出菱形的左边
+			for(q=1;q<=2*i-3;q++){
+				System.out.print(" ");//输出第二段空格   
+			}
+			if (i==1) {
+				System.out.println("");
+			}
+			else {
+				System.out.println("*");
+			}
+
+		}
+		for(i=1;i<=f2;i++){			//共有f2次循环 ，用来刻画下半部分
+			for(m=0;m<i;m++){
+				System.out.print(" ");
+			}
+			System.out.print("*");  //输出菱形的左边
+			for(q=1;q<=(f2-i)*2-1;q++){
+				System.out.print(" ");
+			}
+			if(i==f2){
+				System.out.println("");
+			}
+			else {
+				System.out.println("*");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int  n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please input the Diamond layer :");
+		n = sc.nextInt();
+		qqqq qqqq =new qqqq();
+		qqqq.Diamond(n);
+	}
+}
